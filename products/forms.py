c2ec98a38d20944a19af6ff726bdcd0d4f7fb31f
singeroi <bonjour_robert@outlook.com>
2020-09-24 17:59:12 +0200

    adds Product Admin - adds product view, url and template

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from models import Product, Category
+#from .widgets import CustomClearableFileInput
+from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
@@ -7,6 +8,10 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    '''image = forms.ImageField(label='Image',
+                            required=False,
+                            widget=CustomClearableFileInput)'''
 
     def __init__(self, *args,**kwargs):
         super().__init__(*args,**kwargs)
@@ -16,3 +21,4 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-5'
+
