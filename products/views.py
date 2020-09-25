439e299c2f766ef4b87c67ab0141a233b7cd7fcb
singeroi <bonjour_robert@outlook.com>
2020-09-25 17:50:12 +0200

    Product admin - adds delete products, adds edit delete buttons

--- products/views.py
+++ products/views.py
@@ -92,9 +92,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES )
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product! PLease ensure the form is valid.')
     else:
@@ -106,6 +106,7 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
 
 def edit_product(request, product_id):
     '''Edit a title in the store'''
@@ -129,4 +130,12 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    '''Delete a title from the store'''
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Deleted title.')
+    return redirect(reverse('products'))
 
